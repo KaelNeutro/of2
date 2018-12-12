463c8ef387031958ac317bdb461417a5841cc5da
Priscylla Rodrigues <priscyllarsousa@gmail.com>
2018-12-12 10:04:47 -0200

    tradução home

--- user/homeU.php
+++ user/homeU.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- CSS--> 
     <link type="text/css" rel="stylesheet" href="..\css\style.css">
     <link href="..\css\style.css" rel="stylesheet" id="bootstrap-css">
@@ -21,52 +21,51 @@
     <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBGwgKtk8lwTal0Ng_UkPU6JmqhDKtbmf0">
 </script> -->
-<title>HOME</title>
+<title>Alter Students Data</title>
 </head>
-<body class="container">
-    <div class="row">
+<body class="container" style="background-color: white!important;">
+    <div class="row" style="background-color: white;">
         <div class="center-align">
             <ul class="nav ">
-                <li><a href="homeU.php">Home</a></li> 
+                <li><a href="#">Inicio</a></li> 
 
 
-                <li><a href="#">Students</a>
-                    <ul class ="sub">
-                        <li><a href="reg_std.php"> Register </a></li> 
-                        <li><a href="alter_std.php"> Alter Data</a></li>
-                        <li><a href="view_std.php"> View or Remove </a></li>
+                <li><a href="#">Estudantes</a>
+                    <ul class ="sub" style="background-color: white!important;">
+                        <li><a href="reg_std.php"> Registrar </a></li> 
+                        <li><a href="alter_std.php"> Alterar Dados</a></li>
+                        <li><a href="view_std.php"> Listar ou Remover </a></li>
                     </ul>
                 </li>
 
 
-                <li><a href="#">Vacancies</a>
-                    <ul class ="sub">
-                        <li><a href="search_vacancy.php"> <span class="glyphicon glyphicon-search"> </span>Search</a></li> 
-                        <li><a href="sl_pd_std.php?sit=pending">Pending</a></li>
-                        <li><a href="sl_pd_std.php?sit=accepted">Accepted</a></li>
-                        <li><a href="sl_pd_std.php?sit=canceled">Canceled</a></li>
-                        <li><a href="sl_pd_std.php?sit=declined">Declined</a></li>
+                <li><a href="#">Vagas</a>
+                    <ul class ="sub" style="background-color: white!important;">
+                        <li><a href="search_vacancy.php"> <span class="glyphicon glyphicon-search"> </span>Pesquisar</a></li> 
+                        <li><a href="sl_pd_std.php?sit=pending">Pendentes</a></li>
+                        <li><a href="sl_pd_std.php?sit=accepted">Aprovadas</a></li>
+                        <li><a href="sl_pd_std.php?sit=canceled">Canceladas</a></li>
+                        <li><a href="sl_pd_std.php?sit=declined">Recusadas</a></li>
                     </ul>
                 </li> 
 
 
-                <li><a href="#">User</a>
-                    <ul class ="sub">
-                        <li><a href="alter_u.php" > Edit Account </a></li> 
-                        <li><a href="delfulluser.php"> Remove Account </a></li>
+                <li><a href="#">Usuários</a>
+                    <ul class ="sub" style="background-color: white!important;">
+                        <li><a href="alter_u.php" > Editar Conta </a></li> 
+                        <li><a href="delfulluser.php"> Remover Conta </a></li>
 
                     </ul> 
                 </li>
 
 
 
-                <li><a href="map_school.php"> School</a></li>  
-<li style="background-color: red;"><a href="../Logout.php"> Logout </a></li>
+                <li><a href="menu.php" id="mapschool">Fale Conosco</a></li> 
                 
             </ul>
         </div>
     </div>  
-    <div class="" style="height: 200px;">
+    <div class="" style="height: 200px; " >
         <div id="" class="col-md-8 col-md-offset-2" style="margin-top: 70px;">
           <table><td><img src="../img/logo.png"></td> <td><h4 style="color:white; text-align: center;">"Bem vindo ao Vagas Escolares!
 Nossa missão é facilitar a procura de vagas, tendo como público alvo a população, que encaram filas imensas e em determinadas ocasiões não encontram a vaga desejada. Os usuários cadastrados terão acesso à todas escolas da sua região e participarão do processo de obtenção das vagas disponíveis. As escolas terão como função disponibilizar ao sistema as vagas disponíveis."
